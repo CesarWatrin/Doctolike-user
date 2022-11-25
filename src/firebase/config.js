a329c7cfb228cae8afe56d50ed6243d258d4d95b
CesarWatrin <cesar.watrin@hotmail.com>
2022-11-25 12:45:39 +0100

    export env

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCwUs4IdJ9Xy3_r3S51KjsSXQD_NGoi2w8",
-  authDomain: "doctolike-cdc2a.firebaseapp.com",
-  projectId: "doctolike-cdc2a",
-  storageBucket: "doctolike-cdc2a.appspot.com",
-  messagingSenderId: "164599579913",
-  appId: "1:164599579913:web:217e4ca1286fd7f0b17fd8",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
