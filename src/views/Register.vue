f3b8da506c07b680c2735780a2d90d53f26fb39e
CesarWatrin <cesar.watrin@hotmail.com>
2022-11-25 16:08:27 +0100

    feat(register): adding new field

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -20,7 +20,7 @@ const onRegister = async () => {
       newUser.value = r.user.uid;
       window.navigator.vibrate(200);
       const result = await setDoc(doc(db, "client", newUser.value), {
-        first_name: "Karim",
+        first_name: "",
       });
     }
   } catch (e) {
@@ -33,47 +33,118 @@ const onRegister = async () => {
 
 <template>
   <div class="w-full h-full flex justify-center items-center">
-    <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-      <div class="mb-4">
-        <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
-          Email
-        </label>
-        <input
-          v-model="email"
-          :class="hasError && 'border-red-500'"
-          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-          id="email"
-          type="text"
-          placeholder="email"
-        />
+    <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-wrap">
+      <div class="mr-4">
+        <div class="mb-4">
+          <label
+            class="block text-gray-700 text-sm font-bold mb-2"
+            for="firstName"
+          >
+            First name
+          </label>
+          <input
+            v-model="firstName"
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="firstName"
+            type="text"
+            placeholder="first name"
+          />
+        </div>
+        <div class="mb-4">
+          <label
+            class="block text-gray-700 text-sm font-bold mb-2"
+            for="lastName"
+          >
+            Last name
+          </label>
+          <input
+            v-model="lastName"
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="lastName"
+            type="text"
+            placeholder="last name"
+          />
+        </div>
+        <div class="mb-4">
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
+            Email
+          </label>
+          <input
+            v-model="email"
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="email"
+            type="email"
+            placeholder="email"
+          />
+        </div>
+        <div class="mb-4">
+          <label
+            class="block text-gray-700 text-sm font-bold mb-2"
+            for="password"
+          >
+            Password
+          </label>
+          <input
+            v-model="password"
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+            id="password"
+            type="password"
+            placeholder="password"
+          />
+        </div>
+        <div class="flex items-center justify-between">
+          <button
+            class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            type="button"
+            @click="onRegister"
+          >
+            Register
+          </button>
+        </div>
       </div>
-      <div class="mb-6">
-        <label
-          class="block text-gray-700 text-sm font-bold mb-2"
-          for="password"
-        >
-          Password
-        </label>
-        <input
-          v-model="password"
-          class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
-          :class="hasError && 'border-red-500'"
-          id="password"
-          type="password"
-          placeholder="password"
-        />
-        <p v-if="hasError" class="text-red-500 text-xs italic pt-2">
-          Username or password is incorrect.
-        </p>
-      </div>
-      <div class="flex items-center justify-between">
-        <button
-          class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
-          type="button"
-          @click="onRegister"
-        >
-          Register
-        </button>
+      <div class="ml-4">
+        <div class="mb-4">
+          <label
+            class="block text-gray-700 text-sm font-bold mb-2"
+            for="address"
+          >
+            Address
+          </label>
+          <input
+            v-model="address"
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="address"
+            type="text"
+            placeholder="address"
+          />
+        </div>
+        <div class="mb-4">
+          <label
+            class="block text-gray-700 text-sm font-bold mb-2"
+            for="zipcode"
+          >
+            Zipcode
+          </label>
+          <input
+            v-model="zipcode"
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="zipcode"
+            type="text"
+            placeholder="zipcode"
+          />
+        </div>
+        <div class="mb-4">
+          <label class="block text-gray-700 text-sm font-bold mb-2" for="town">
+            Town
+          </label>
+          <input
+            v-model="town"
+            class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            id="town"
+            type="text"
+            placeholder="town"
+          />
+        </div>
       </div>
     </form>
   </div>
