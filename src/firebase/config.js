b3257da0dc0bc871119b5b8ad3210331ce08f333
CesarWatrin <cesar.watrin@hotmail.com>
2022-12-16 11:55:41 +0100

    feat: try setup push notification

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
+import { getMessaging, getToken } from "firebase/messaging";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -14,5 +15,10 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 const auth = getAuth(app);
+const messaging = getMessaging(app);
+getToken(messaging, {
+  vapidKey:
+    "BO70gXqyzSf1aEvmO0NIs_CXJjBNMGAwtxijV7ynmUOVil538KRW1FTLCNIpcBkzDm5pzpKJxhjAH1ZwL090vsA",
+});
 
-export { db, auth };
+export { db, auth, messaging };
